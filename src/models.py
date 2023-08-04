1a0549072f65282c4ef1be620c4d1fd269882793
Rodney Gauna <rodneygauna@gmail.com>
2023-08-04 21:48:47 +0000

    request model added

--- src/models.py
+++ src/models.py
@@ -38,7 +38,7 @@ class User(db.Model, UserMixin):
     # Timestamps
     created_date = db.Column(db.DateTime, nullable=False,
                              default=datetime.utcnow)
-    updated_date = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_date = db.Column(db.DateTime)
     # Status
     status = db.Column(db.String(10), default="ACTIVE")
     # Profile Picture
@@ -69,4 +69,83 @@ class HealthApp(db.Model):
     # Timestamps
     created_date = db.Column(db.DateTime, nullable=False,
                              default=datetime.utcnow)
-    updated_date = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_date = db.Column(db.DateTime)
+    # Updatestamps
+    created_by = db.Column(db.Integer, db.ForeignKey("users.id"))
+    updated_by = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+
+# Model - Connection Request
+class ConnectionRequest(db.Model):
+    """Connection Request model"""
+
+    __tablename__ = "connection_requests"
+
+    # IDs
+    id = db.Column(db.Integer, primary_key=True)
+    # Requester Information
+    firstname = db.Column(db.String(255), nullable=False)
+    lastname = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    phone_number = db.Column(db.String(255), nullable=False)
+    company = db.Column(db.String(255), nullable=False)
+    company_website = db.Column(db.String(255), nullable=False)
+    # Application Information
+    app_name = db.Column(db.String(255), nullable=False)
+    app_link = db.Column(db.String(255), nullable=False)
+    app_type = db.Column(db.Text, nullable=False)
+    app_description = db.Column(db.Text, nullable=False)
+    carin_link = db.Column(db.Text)
+    medicare_link = db.Column(db.Text)
+    caqh_link = db.Column(db.Text)
+    # Requesting Information
+    fhir_api = db.Column(db.Text, nullable=False)
+    # Health Plan Information
+    health_plan_name = db.Column(db.Text, nullable=False)
+    # Timestamps
+    created_date = db.Column(db.DateTime, nullable=False,
+                             default=datetime.utcnow)
+
+
+# Model - Request Jira Ticket
+class RequestJira(db.Model):
+    """Request Jira relationship model"""
+
+    __tablename__ = "request_jira"
+
+    # IDs
+    id = db.Column(db.Integer, primary_key=True)
+    connectionrequest_id = db.Column(
+        db.Integer, db.ForeignKey("connection_requests.id"))
+    # Jira Information
+    jira_id = db.Column(db.String(255), nullable=False)
+    jira_url = db.Column(db.String(255), nullable=False)
+    jira_status = db.Column(db.String(255))
+    # Timestamps
+    created_date = db.Column(db.DateTime, nullable=False,
+                             default=datetime.utcnow)
+    updated_date = db.Column(db.DateTime)
+    # Updatestamps
+    created_by = db.Column(db.Integer, db.ForeignKey("users.id"))
+    updated_by = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+
+# Model - Request Working Status
+class RequestWorkingStatus(db.Model):
+    """Request working status model"""
+
+    __tablename__ = "request_working_status"
+
+    # IDs
+    id = db.Column(db.Integer, primary_key=True)
+    connectionrequest_id = db.Column(
+        db.Integer, db.ForeignKey("connection_requests.id"))
+    # Working Status Information
+    working_status = db.Column(db.String(255), nullable=False)
+    # Timestamps
+    created_date = db.Column(db.DateTime, nullable=False,
+                             default=datetime.utcnow)
+    updated_date = db.Column(db.DateTime)
+    # Updatestamps
+    created_by = db.Column(db.Integer, db.ForeignKey("users.id"))
+    updated_by = db.Column(db.Integer, db.ForeignKey("users.id"))
